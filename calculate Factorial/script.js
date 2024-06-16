1fb3f9a332a0dabb68b702ea6622bae055961d62
AbdulRahmanAzam <azamabdulrahman930@gmail.com>
2024-06-16 17:31:13 +0500

    exercise 11

--- calculate Factorial/script.js
+++ calculate Factorial/script.js
@@ -1,9 +1,23 @@
 // 6! = 1 x 2 x 3 x 4 x 5 x 6
 
-let no = prompt("Enter a number to calculate Factorial");
+// let no = prompt("Enter a number to calculate Factorial");
 
-let factorial = 1;
-for(let i = 1; i <= no; i++)
-    factorial *= (i);
+// Approach 1
+// let factorial = 1;
+// for(let i = 1; i <= no; i++)
+//     factorial *= (i);
 
-alert(factorial);
+// alert(factorial);
+
+// Approach 2
+let no = 6;
+function factorial(no){
+    let arr = Array.from(Array(no + 1).keys());
+    console.log(arr.slice(1, 2));
+    let c = arr.slice(3).reduce((a, b) => {
+        return a*b;
+    })
+    return c
+}
+
+console.log(factorial(no));
